e59c7bb3ad9277ee1b73720e87af11199ecdb6a0
Siteman <andreas@siteman.no>
2014-03-18 18:08:29 +0100

    Added github plugin-updater

--- veuse-portfolio.php
+++ veuse-portfolio.php
@@ -754,6 +754,28 @@ class VeusePortfolio {
 $veuse_portfolio = new VeusePortfolio;
 
 
+/* Updater */
+
+include_once('updater.php');
+
+if ( is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        'proper_folder_name' => 'veuse-portfolio', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/veuse/veuse-portfolio', // the github API url of your github repo
+        'raw_url' => 'https://raw.github.com/veuse/veuse-portfolio/master', // the github raw url of your github repo
+        'github_url' => 'https://github.com/veuse/veuse-portfolio', // the github url of your github repo
+        'zip_url' => 'https://github.com/veuse/veuse-portfolio/zipball/master', // the zip url of the github repo
+        'sslverify' => false, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '3.7', // which version of WordPress does your plugin require?
+        'tested' => '3.8', // which version of WordPress is your plugin tested up to?
+        'readme' => 'README.md', // which file to use as the readme for the version number
+        'access_token' => '' // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
+    );
+    new WP_GitHub_Updater($config);
+}
+
+
 require_once('widget.php');
 
 /* Pagination */
